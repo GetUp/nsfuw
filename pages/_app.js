03ae6b0dcaeab1de0253b833c806146866041e6b
nirentuladhar <nirentuladhar@gmail.com>
2021-03-11 10:16:39 +1100

    Setup GA for pageviews

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import "../styles.css";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+import '../styles.css'
 
 export default function CustomApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  return <Component {...pageProps} />
 }
