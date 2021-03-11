e383d2c8cf40fd0207f27a3a04f8658a61dde1f6
nirentuladhar <nirentuladhar@gmail.com>
2021-03-11 14:20:30 +1100

    Add more text sizes

--- components/StoryImage.js
+++ components/StoryImage.js
@@ -3,36 +3,46 @@ import { nameFromCode } from 'lib/services'
 const StoryHeader = ({ serviceCode }) => {
   const name = nameFromCode(serviceCode)
   return (
-    <div className="flex justify-between items-center bg-gray-800 p-4 h-full text-white">
+    <div className="flex justify-between items-center bg-gray-800 p-4 h-16 text-white">
       <div className="text-xl font-medium">My {name} story</div>
-      <img src={`/${serviceCode}.png`} alt={`${name} logo`} className="w-1/3 object-contain" style={{ maxWidth: '150px' }} />
+      <img
+        src={`/${serviceCode}.png`}
+        alt={`${name} logo`}
+        className="w-1/3 object-contain"
+        style={{ maxWidth: '150px' }}
+      />
     </div>
   )
 }
 
 export default function StoryImage({ shareImageId = 'not-the-share-image', serviceCode, story }) {
-  return (<>
-    {shareImageId === 'not-the-share-image' ? (
-      <div className="bg-gray-900">
-        <div className="max-w-screen-xl mx-auto py-16 px-4 ">
-          <div id={shareImageId} className="bg-secondary-500 rounded-sm border border-gray-700">
-            <StoryHeader {...{ serviceCode }} />
+  return (
+    <>
+      {shareImageId === 'not-the-share-image' ? (
+        <div className="bg-gray-900">
+          <div className="max-w-screen-xl mx-auto py-16 px-4 ">
+            <div id={shareImageId} className="bg-secondary-500 rounded-sm border border-gray-700">
+              <StoryHeader {...{ serviceCode }} />
 
-            <div className="text-white text-2xl p-4">{story}</div>
+              <div className="text-white text-2xl p-4">{story}</div>
 
-            <div className="p-4 text-gray-900 font-bold tracking-tight">#80ADAY #BTPM #NSFUW</div>
+              <div className="p-4 text-gray-900 font-bold tracking-tight">#80ADAY #BTPM #NSFUW</div>
+            </div>
           </div>
         </div>
-      </div>
       ) : (
         <div id={shareImageId}>
-          <div className="bg-secondary-500">
+          <div className="bg-secondary-500 h-full">
             <StoryHeader {...{ serviceCode }} />
-
-            <div className="p-4">{story}</div>
-            <div className="p-4 text-gray-900 font-bold tracking-tight text-base">#80ADAY #BTPM #NSFUW</div>
+            <div className="h-full">
+              <div className="p-4 text-white" id="story">
+                {story}
+              </div>
+              <div className="p-4 text-gray-900 font-bold tracking-tight text-base">#80ADAY #BTPM #NSFUW</div>
+            </div>
           </div>
         </div>
       )}
-  </>)
+    </>
+  )
 }
